033831fc8c0fd68ce017adefb51bf2592f803923
k2works <kakimomokuri@gmail.com>
2018-12-01 16:49:10 +0900

    test: 3つ目のテストを作成

--- dev/sam-app/tests/unit/fizz_buzz/fizz_buzz_test.rb
+++ dev/sam-app/tests/unit/fizz_buzz/fizz_buzz_test.rb
@@ -13,4 +13,9 @@ class FizzBuzzTest < Test::Unit::TestCase
     assert_equal('Buzz', FizzBuzz.generate(5))
     assert_not_equal('Buzz', FizzBuzz.generate(1))
   end
+
+  test '15ならFizzBuzzを返す' do
+    assert_equal('FizzBuzz', FizzBuzz.generate(15))
+    assert_not_equal('FizzBuzz', FizzBuzz.generate(1))
+  end
 end
